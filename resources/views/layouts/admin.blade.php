02cf58c73958bc1a139d7f0a9d101992bdc44bb3
z-Nava <62721391+z-Nava@users.noreply.github.com>
2025-05-05 10:20:51 -0600

    mw image

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -7,13 +7,14 @@
 </head>
 <body class="bg-animated text-white min-h-screen">
     <div class="bg-black/80 min-h-screen flex flex-col">
-        <header class="p-6 bg-red-600 shadow-lg">
-            <h1 class="text-3xl font-bold">Panel de Administrador</h1>
+        <header class="p-6 bg-red-600 shadow-lg flex items-center space-x-6">
+            <img src="{{ asset('images/MW-LOGO-WHITE.png') }}" alt="Logo" class="w-16 h-16">
+            <h1 class="text-3xl font-bold">Panel de Administrador</h1>  
         </header>
-
         <main class="flex-1 p-10">
             @yield('content')
         </main>
     </div>
 </body>
 </html>
+
